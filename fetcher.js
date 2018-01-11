d5ac9b1e0841c606a9940bc3273331f2af818c76
Vishal Swarnkar <vishal.swarnkar.ext@bbc.co.uk>
2018-01-11 12:14:55 +0000

    update outcome.json with max resource count
    removed access details from fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,3 @@
-// var fetch = require('promise-path').fetch
 var request = require('request-promise');
 var url = "https://bbcpodtest.testrail.com/index.php?/api/v2/get_plans/"
 var apikey = 'your testrail apikey'
