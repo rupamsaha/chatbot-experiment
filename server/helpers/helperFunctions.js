f1cf543bb8b905761766800ddce0234f331aa495
Asim Khan <Asimk21@users.noreply.github.com>
2018-01-18 19:59:52 +0000

    Function to get test case id from mapping file

---
+++ server/helpers/helperFunctions.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var unique = require('array-unique');
+
+function getTestCaseIdsFromMappingFile(updatedFiles, mappingFilePath, callback){
+  var testCaseIds = []
+  fs.readFile(mappingFilePath, 'utf8', function(err, contents) {
+    contents = JSON.parse(contents)
+    updatedFiles.forEach(function(file){
+      if(contents[file] != undefined){
+        testCaseIds.push(contents[file])
+      }
+    })
+  callback(unique([].concat.apply([], testCaseIds)))
+  });
+}
