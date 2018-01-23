331d2d1973eab5ebaf3c11a2fad5e3e16114e13f
Vishal Swarnkar <vishal.swarnkar.ext@bbc.co.uk>
2018-01-23 23:49:55 +0000

    removed access details

--- server/helpers/testrail.js
+++ server/helpers/testrail.js
@@ -3,8 +3,8 @@ var testrail_resp = '';
 
 var testrail = new Testrail({
   host: 'https://bbcpodtest.testrail.com',
-  user: 'vishal.swarnkar.ext@bbc.co.uk' || Process.env.TESTRAILUSER,
-  password: 'RV1O5pOmApCJXb7Bj/M8-RVbXUDZcSneM4aph5A44' || Process.env.TESTRAILPASSWORD
+  user: username || Process.env.TESTRAILUSER,
+  password: password || Process.env.TESTRAILPASSWORD
 });
 
 function addRunInTestRail(projectID, options, callback){
