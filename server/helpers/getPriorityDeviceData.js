d44172219af103d3ce1eee8d02c66a9d5daa4031
Rupam Saha <rupam.saha.ext@bbc.co.uk>
2018-04-05 13:42:56 +0100

    Code to fetch top 10 devices

---
+++ server/helpers/getPriorityDeviceData.js
@@ -0,0 +1,21 @@
+const HttpClientClass = require('@bbc/http-client')
+const httpClient = new HttpClientClass()
+const _ = require('lodash')
+
+const url = "https://tap-dashboard.tools.bbc.co.uk/graphs/devices/top/past_week"
+
+options = {
+  method: 'GET',
+  uri: 'https://tap-dashboard.tools.bbc.co.uk/graphs/devices/top',
+  timeout: 100000
+}
+
+httpClient.get(options, function(error, response, data) {
+  if (!error && response.statusCode == 200) {
+      var myRegexp = /var results =(...+)/
+      var match = myRegexp.exec(data)
+      _.take(JSON.parse(match[1]), 10).map(function (obj) {
+          console.log(obj.deviceKey)
+      })
+  }
+})
